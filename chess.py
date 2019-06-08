2bafe147cd36ff983cbef456afd95b5e257e851c
ZeroOne010101 <43586679+ZeroOne010101@users.noreply.github.com>
2019-06-08 21:44:56 +0200

    failed at making move class, started making player class in order to actually thest the stuff im implementing

--- chess.py
+++ chess.py
@@ -5,6 +5,10 @@ pprint.PrettyPrinter(indent=4)
 
 
     
+
+
+
+    
 class Board:
     def __init__ (self):
         self.board = []
@@ -76,71 +80,89 @@ class Pawn(Piece): # How!?!?!?
     def __init__(self, square):
         self.square = square.Square
         
+class Player:
+    def __init__(self, player1, player2): #my brain is f***ed, continue here tomorrow when you've thought of how to get the current move and player. the whites and blacks move is meant for printing a players last move
+        self.white = 'white' #player1
+        self.black = 'black' #player2
+        self.turn_var = self.white
+        self.whites_move = None
+        self.blacks_move = None
+    def input(self):
+        self.userinput = input(f'{self.turn_var}\'s turn: ')
+        if self.turn_var == self.white:
+            self.whites_move = self.userinput
+            self.turn_var = self.black
+        else:
+            self.blacks_move = self.userinput
+            self.turn_var = self.white
+    def last_move_white(self):
+        return self.whites_move
+    def last_move_black(self):
+        return self.blacks_move
 
 
+### REDO THIS MESS !!! ###
 
+# class Move:
+#     def __init__(self, row, column):
+#         pass
 
+#     def move_piece(self, square):
+#         pass
 
-class Move:
-    def __init__(self, row, column):
-        pass
+#     def can_move(self, square, move_row, move_column): # currently trying to remake this into classes, since this doesnt really seem to be a good way to go about things
 
-    def move_piece(self, square):
-        pass
-
-    def can_move(self, square, move_row, move_column): # currently trying to remake this into classes, since this doesnt really seem to be a good way to go about things
-
-        # None Type (to prevent None Type pieces from being moved, needs to be at the top)
-        if square.role == None:
-            return False
+#         # None Type (to prevent None Type pieces from being moved, needs to be at the top)
+#         if square.role == None:
+#             return False
         
-        # Pawns
-        #ToDo: add diagonal movement if pieces, prevent straightt movement of pieces if another piece blocks the path 
-        if sqare.role == 'Pawn':
-            if square.color == 'black':
-                for row in Board.board:
+#         # Pawns
+#         #ToDo: add diagonal movement if pieces, prevent straightt movement of pieces if another piece blocks the path 
+#         if sqare.role == 'Pawn':
+#             if square.color == 'black':
+#                 for row in Board.board:
                     
-                    if move_row == square.row-1:
-                        if move_column == square.column-1 or move_column == square.column +1:
-                            for column in Board.board:
-                                pass
+#                     if move_row == square.row-1:
+#                         if move_column == square.column-1 or move_column == square.column +1:
+#                             for column in Board.board:
+#                                 pass
 
-                        return True
-                    if square.row == 6 & move_row == square.row-2:
-                        return True
-                    else:
-                        return False
-            else: 
-                for row in Board.board:
-                    if move_row == square.row+1:
-                        return True
-                    if square.row == 1 & move_row == square.row+2:
-                        return True
-                    else:
-                        return False
+#                         return True
+#                     if square.row == 6 & move_row == square.row-2:
+#                         return True
+#                     else:
+#                         return False
+#             else: 
+#                 for row in Board.board:
+#                     if move_row == square.row+1:
+#                         return True
+#                     if square.row == 1 & move_row == square.row+2:
+#                         return True
+#                     else:
+#                         return False
 
-        # Rooks 
-        #ToDo: implement the king and rook switching places if neither of them have moved since beginning of the match
-        if sqare.role == 'Rook':
-
-
-            if square.color == 'black':
-                for row in Board.board:
-                    if move_row == square.row or move_column == square.column:
-                        return True
-                    else:
-                        return False
-            else:
-                return False
+        # # Rooks 
+        # #ToDo: implement the king and rook switching places if neither of them have moved since beginning of the match
+        # if sqare.role == 'Rook':
 
 
+        #     if square.color == 'black':
+        #         for row in Board.board:
+        #             if move_row == square.row or move_column == square.column:
+        #                 return True
+        #             else:
+        #                 return False
+        #     else:
+        #         return False
 
 
 
-        
+
+
 
 def main():
-    Board()
+    #do_input_stuff()/chess_init
+    pass   
 
 if __name__ == '__main__':
     main()
