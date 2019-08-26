e0f3ca04e2918c11aac1d20d0210c0fc1666d647
ZeroOne010101 <43586679+ZeroOne010101@users.noreply.github.com>
2019-08-27 00:30:34 +0200

    Split chess.py into board.py and main.py in prereration for the upcoming stage 2. Tried my hand at module specific logging, but failed.

--- board.py
+++ board.py
@@ -2,7 +2,11 @@ from chess_pieces import *
 import re
 import logging 
 
-logging.getLogger().setLevel(logging.WARNING)
+logging.getLogger().setLevel(logging.DEBUG)
+
+#board_logger = logging.getLogger(__name__)
+#board_logger.setLevel(logging.DEBUG)
+
 
 # small helper functions
 def subtract(tup): return tup [0] - tup [1]
@@ -224,38 +228,4 @@ class Board:
         ) 
 
         logging.debug(f"Move {move} interpreted as {(origin, destination)}")
-        return Move (origin, destination, flip = self.turn == WHITE) 
-
-def main():
-    '''
-    The loop the game runs in.
-    It takes a user input, checks that input for validity, and calls move on it if it is valid.
-    It terminates once the game is won or a player quits the game.
-    '''
-    board = Board()
-    
-    while True:
-        print (board)
-        move_valid = False  # makes the input loop run at least once
-        move = None
-        while not move_valid:
-            pinput = input(f'{board.turn}: ').lower()  # .lower() to make everything lowercase
-            
-            if pinput in ['quit', 'exit']:
-                print(f'{board.turn} quit the game.')
-                return 0
-              
-            move = board.getMove(pinput)
-            
-            if move is not None:
-                move_valid = board.is_valid_move(move)
-            if not move_valid:
-                print(f'Move [{pinput}] is not valid.')
-                
-            logging.debug(f"Move valid? {move_valid}")
-            
-        board.move(move)
-               
-# Makes sure, that the game only runs when it is not being imported.
-if __name__ == '__main__':
-    main()
+        return Move (origin, destination, flip = self.turn == WHITE)
