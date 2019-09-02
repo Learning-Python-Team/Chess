98c9580826ec3915b26cbce46a1566882269c029
Jp Unknown <jp.unknown47@gmail.com>
2019-09-02 10:21:43 -0400

    Beginning  Pygame

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
 from chess_pieces import *
 from board import *
 import logging
+import pygame
 
-#logging.getLogger().setLevel(logging.DEBUG)
+# logging.getLogger().setLevel(logging.DEBUG)
 
-#main_logger = logging.getLogger('main')
-#main_logger.setLevel(logging.DEBUG)
+# main_logger = logging.getLogger('main')
+# main_logger.setLevel(logging.DEBUG)
+
 
 def main():
     '''
     The loop the game runs in.
-    It takes a user input, checks that input for validity, and calls move on it if it is valid.
+    It takes a user input, checks that input for validity, and calls move_input on it if it is valid.
     It terminates once the game is won or a player quits the game.
     '''
+    
+    window_size = 100, 100
+    pygame.init()
+    screen = pygame.display.set_mode(window_size)
+    
+    font = pygame.font.SysFont('Arial', 20)
+    
     board = Board()
     
     while True:
-        print (board)
+        print(board)
+        for row in Board().game_board:
+            for square in row:
+                print(square.row, square.column, square.piece)
+        
         move_valid = False  # makes the input loop run at least once
         move = None
         while not move_valid:
@@ -36,6 +49,7 @@ def main():
             logging.debug(f"Move valid? {move_valid}")
             
         board.move(move)
+        pygame.display.flip()
                
 # Makes sure, that the game only runs when it is not being imported.
 if __name__ == '__main__':
